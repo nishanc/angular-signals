0dfe7abbfaab1540f69004ffbe6f6f91c27ba944
Nishan Wickramarathna <inbox.nishan@gmail.com>
2023-12-18 00:54:55 +0530

    image upload component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { EMPTY, catchError, finalize } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
 
-  constructor() { }
+export class ProfileComponent {
+  imageUrl: string = 'https://i.imgur.com/o9fpo46.png'; // Default image URL
+  fetchedImageUrl: string = ''; // Fetched image URL
+  isLoading: boolean = false; // Loading indicator
 
-  ngOnInit() {
+  constructor(private http: HttpClient) {}
+
+  saveImage() {
+    this.isLoading = true;
+    this.http.get(this.imageUrl, { responseType: 'blob' }).pipe(
+      catchError(() => {
+        console.error('Error fetching image');
+        return EMPTY; // Return an empty observable to continue the stream
+      }),
+      finalize(() => this.isLoading = false) // Set isLoading to false when the request completes (success or error)
+    ).subscribe((response: Blob) => {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        this.fetchedImageUrl = reader.result as string;
+      };
+      reader.readAsDataURL(response);
+    });
   }
-
 }
